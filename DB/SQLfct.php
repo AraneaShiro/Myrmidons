5fbbba70a6d024271d523759cd76d6d1074127a9
Maxence <alexmax6262v2@gmail.com>
2026-03-26 16:13:38 +0100

    update fonction sql
    on peut rechercher toutes les recettes
    ou en rajouter dans la BDD
    
    + rajout du caractère unique pour les noms des ingrédients et des tags

--- DB/SQLfct.php
+++ DB/SQLfct.php
@@ -2,34 +2,82 @@
 
 class SQLfct {
 
+    private $db_name = "myrmidons";
+    private $db_host = "127.0.0.1";
+    private $db_port = "3306";
+
+    private $db_user = "root";
+    private $db_pwd = "root";
+
     private $pdo;
 
 public function __construct() {
 
     try { 
-        $db_name = "myrmidons" ; $db_host = "127.0.0.1" ; $db_port = "3306" ;
-        $db_user = "root" ; $db_pwd = "root" ;
-        $dsn = 'mysql:dbname=' . $db_name . ';host='. $db_host. ';port=' . $db_port;
-        $pdo = new PDO($dsn, $db_user, $db_pwd) ;
+
+        $dsn = 'mysql:dbname=' . $this->db_name . ';host='. $this->db_host. ';port=' . $this->db_port;
+
+        $this->pdo = new PDO($dsn, $this->db_user, $this->db_pwd) ;
+
     } catch(\Exception $ex){
-        die("Erreur : " . $ex->getMessage()) ;
+        die("Erreur connexion BDD : " . $ex->getMessage()) ;
 }
 
 }
 
-//fonction qui rajouter dans la BDD une recette
-function ajouterRecette($nomRecette, $descriptionRecette, $photo) {
+/* 
+=================================================
+    * FONCTIONS POUR RECHERCHER DANS LA BDD *
+=================================================
+*/
 
-    $query = "INSERT INTO recette (name, description, photo) VALUES (:nom, :desc, :photo)" ;
-    $statement = $pdo->prepare($query);
+/**
+ * fonction qui donne toutes les recettes existantes
+ */
+function rechercherRecettesAll() {
 
+    try {
+    $query =    "SELECT * FROM recette 
+                ORDER BY nom DESC" ;
+
+    $statement = $this->pdo->prepare($query);
+
+    $statement->execute();
+
+    return $statement->fetchAll();
+    } catch(\Exception $ex){
+        die("Erreur rechercherRecetteAll : " . $ex->getMessage()) ;
+}
+}
+
+/* 
+=================================================
+ * FONCTIONS POUR CREER OU MODIFIER DANS LA BDD *
+=================================================
+ */
+
+/**
+ * fonction qui rajoute dans la BDD une recette avec son nom, texte et photo
+ */
+function ajouterRecette($nomRecette, $texteRecette, $photoRecette) {
+
+    try {
+    $query = "INSERT INTO recette (nom, texte, photo) VALUES (:nom, :texte, :photo)" ;
+
+    $statement = $this->pdo->prepare($query);
+
+    // remplacement des valeurs avec les parametres
     $statement->bindValue(':nom', $nomRecette) ;
-    $statement->bindValue(':desc', $descriptionRecette) ;
-    $statement->bindValue(':photo', $photo) ;
+    $statement->bindValue(':texte', $texteRecette) ;
+    $statement->bindValue(':photo', $photoRecette) ;
 
-    $statement->execute() or die(var_dump($statement->errorInfo())) ;
+    $statement->execute();
 
+    } catch(\Exception $ex){
+        die("Erreur insertion recette : " . $ex->getMessage());
     }
+
+}
 
 }
 
--- DB/myrmidons.sql
+++ DB/myrmidons.sql
@@ -7,22 +7,25 @@ DROP TABLE IF EXISTS tag;
 CREATE TABLE recette (
     recetteID int PRIMARY KEY AUTO_INCREMENT,
     nom varchar(255),
-    texte varchar(255),
+    texte text,
     photo varchar(255)
 );
 
 CREATE TABLE ingredient (
     ingredientID int PRIMARY KEY AUTO_INCREMENT,
-    nom varchar(255),
+    nom varchar(255) UNIQUE, -- on le mettra en unique pour éviter des doublons
     photo varchar(255)
 );
 
 CREATE TABLE tag (
     tagID int PRIMARY KEY AUTO_INCREMENT,
-    nom varchar(255)
+    nom varchar(255) UNIQUE-- on le mettra en unique pour éviter des doublons
 );
 
+-- liaison entre recette & ingredient
 CREATE TABLE recetteIngredient (
+
+    recetteIngredientID int PRIMARY KEY AUTO_INCREMENT,
 
     recetteID int,
     FOREIGN KEY (recetteID)
@@ -34,7 +37,10 @@ CREATE TABLE recetteIngredient (
 
 );
 
+-- liaison entre recette & tag
 CREATE TABLE recetteTag (
+
+    recetteTagID int PRIMARY KEY AUTO_INCREMENT,
 
     recetteID int,
     FOREIGN KEY (recetteID)
@@ -46,14 +52,5 @@ CREATE TABLE recetteTag (
 
 );
 
-
-
-
-
-
-/*
-ALTER TABLE recette
-UPDATE recetteID int NOT NULL AUTO_INCREMENT;
-*/
 
 
